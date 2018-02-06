06f18ff07f01fcfff7102dcb073d9e4ab8327031
Ian Gwillim <igwillim@gmail.com>
2018-02-05 23:44:06 -0500

    Java solution for String Reverse

---
+++ JavaStringReverse/src/javastringreverse/JavaStringReverse.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javastringreverse;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Ian
+ */
+public class JavaStringReverse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. */
+        Scanner in = new Scanner(System.in);
+        String A = in.next();
+        
+        String B = "";
+        ArrayList<Character> list = new ArrayList<>();
+        
+        for (int i = A.length(); i > 0; i--) {
+            list.add(A.charAt(i-1));
+        }
+        
+        for (int i = 0; i < list.size(); i++) {
+            B += list.get(i);
+        }
+        
+        if ( B.equals(A) ) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+    
+}
