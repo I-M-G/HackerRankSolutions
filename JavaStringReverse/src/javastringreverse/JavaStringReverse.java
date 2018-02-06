f09f303371c5b12065cae234a338ecd793033cb7
Ian Gwillim <igwillim@gmail.com>
2018-02-05 23:52:24 -0500

    Added comments to my code

--- JavaStringReverse/src/javastringreverse/JavaStringReverse.java
+++ JavaStringReverse/src/javastringreverse/JavaStringReverse.java
@@ -19,19 +19,25 @@ public class JavaStringReverse {
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. */
         Scanner in = new Scanner(System.in);
+        // Store word to be checked
         String A = in.next();
         
+        // Store reversed string
         String B = "";
+        // Array list of Characters
         ArrayList<Character> list = new ArrayList<>();
         
+        // Store word in reverse to ArrayList
         for (int i = A.length(); i > 0; i--) {
             list.add(A.charAt(i-1));
         }
         
+        // Move ArrayList Characters to new string
         for (int i = 0; i < list.size(); i++) {
             B += list.get(i);
         }
         
+        // Check if word is a palindrome
         if ( B.equals(A) ) {
             System.out.println("Yes");
         }
