fcd94e91b9f3f20c4c8ddf1e6cf6d7e20b951ef2
Ian Gwillim <igwillim@gmail.com>
2018-02-13 23:25:50 -0500

    Added comments and submitted code. Passed!

--- JavaChallenges/SubstringComparisons/src/substringcomparisons/SubstringComparisons.java
+++ JavaChallenges/SubstringComparisons/src/substringcomparisons/SubstringComparisons.java
@@ -21,14 +21,18 @@ public class SubstringComparisons {
         // 'smallest' must be the lexicographically smallest substring of length 'k'
         // 'largest' must be the lexicographically largest substring of length 'k'
         
+        // Store the first section of the string. Use this section for testing
         smallest = s.substring(0, k);
         largest = s.substring(0, k);
         
+        // Loop through the string with the size section we need
         for (int i = 0; i < s.length()-(k-1); i++) {
             
+            // Check if the first section is smaller than the current section
             if ( s.substring(i, i+k).compareTo(smallest) < 0 ) {
                 smallest = s.substring(i, i+k);
             }
+            // Check if the first section is larger than the current section
             else if ( s.substring(i, i+k).compareTo(largest) > 0 ) {
                 largest = s.substring(i, i+k);
             }
@@ -37,7 +41,8 @@ public class SubstringComparisons {
         
         return smallest + "\n" + largest;
     }
-
+    
+    // Main was provided
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String s = scan.next();
