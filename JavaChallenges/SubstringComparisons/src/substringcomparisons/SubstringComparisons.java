84a7e1b2ceb840bba1043cebb59beb9c49e85b6f
Ian Gwillim <igwillim@gmail.com>
2018-02-13 23:20:04 -0500

    Finished a working version of the test case

--- JavaChallenges/SubstringComparisons/src/substringcomparisons/SubstringComparisons.java
+++ JavaChallenges/SubstringComparisons/src/substringcomparisons/SubstringComparisons.java
@@ -22,19 +22,18 @@ public class SubstringComparisons {
         // 'largest' must be the lexicographically largest substring of length 'k'
         
         smallest = s.substring(0, k);
-        // largest = s.substring(0, k);
+        largest = s.substring(0, k);
         
-        for (int i = 0; i < s.length()-k; i++) {
+        for (int i = 0; i < s.length()-(k-1); i++) {
             
-            if ( s.substring(i, k).compareTo(smallest) < 0 ) {
-                System.out.println(s.substring(i, k));
-                smallest = s.substring(i, k);
+            if ( s.substring(i, i+k).compareTo(smallest) < 0 ) {
+                smallest = s.substring(i, i+k);
+            }
+            else if ( s.substring(i, i+k).compareTo(largest) > 0 ) {
+                largest = s.substring(i, i+k);
             }
             
         }
-        
-        
-        
         
         return smallest + "\n" + largest;
     }
