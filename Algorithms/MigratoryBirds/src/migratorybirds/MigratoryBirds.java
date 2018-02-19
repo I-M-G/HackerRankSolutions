21fddf5c9e5ec6d74dd8de23c19f68eafe8ca39f
Ian Gwillim <igwillim@gmail.com>
2018-02-18 21:16:27 -0500

    This is a passing solution to Migratory Birds. I'm sure there is a better way to do this one.

--- Algorithms/MigratoryBirds/src/migratorybirds/MigratoryBirds.java
+++ Algorithms/MigratoryBirds/src/migratorybirds/MigratoryBirds.java
@@ -12,18 +12,25 @@ import java.util.*;
  * @author Ian <https://github.com/I-M-G>
  */
 public class MigratoryBirds {
-
+    /*
+    Not real happy with this solution
+    It works, but I am sure there is a better answer
+    I would like to come back to this one
+    */
     static int migratoryBirds(int n, int[] ar) {
         // Complete this function
-        int type = 0;
+        int type = 5;
+        // Using this array to store the totals of each list
         int[] totals = new int[5];
         
+        // Using lists to store the total of each type
         ArrayList<Integer> typeOne = new ArrayList<>();
         ArrayList<Integer> typeTwo = new ArrayList<>();
         ArrayList<Integer> typeThree = new ArrayList<>();
         ArrayList<Integer> typeFour = new ArrayList<>();
         ArrayList<Integer> typeFive = new ArrayList<>();
         
+        // Go through the given array and store each type in the proper list
         for (int i = 0; i < ar.length; i++) {
             
             switch (ar[i]) {
@@ -48,23 +55,26 @@ public class MigratoryBirds {
             
         }
         
-        System.out.println("1 " + typeOne.size());
-        System.out.println("2 " + typeTwo.size());
-        System.out.println("3 " + typeThree.size());
-        System.out.println("4 " + typeFour.size());
-        System.out.println("5 " + typeFive.size());
+//        System.out.println("1 " + typeOne.size());
+//        System.out.println("2 " + typeTwo.size());
+//        System.out.println("3 " + typeThree.size());
+//        System.out.println("4 " + typeFour.size());
+//        System.out.println("5 " + typeFive.size());
         
-        // I don't like this....
+        // Add the size of each list to the array that will check for the most types
         totals[0] = typeOne.size();
         totals[1] = typeTwo.size();
         totals[2] = typeThree.size();
         totals[3] = typeFour.size();
         totals[4] = typeFive.size();
         
+        // Store the last value to check the other values
+        int temp = totals[totals.length-1];
+        
         for (int i = totals.length-1; i >= 0; i--) {
-            int temp = totals[totals.length-1];
-            System.out.println("totals[i] " + totals[i]);
-            if (totals[i] > temp) {
+            // If we find a bigger value than the temp
+            // Store that value in temp and update the type
+            if (totals[i] >= temp) {
                 temp = totals[i];
                 type = i+1;
             }
@@ -73,6 +83,7 @@ public class MigratoryBirds {
         return type;
     }
     
+    // Main was provided
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
